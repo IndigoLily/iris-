28e03be3c7dc1ef237876e1ae659822c33c247cc
Indigo Lily <yulex.42@gmail.com>
2018-03-29 14:07:34 -0300

    Added animation

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let w = canvas.width  = 512;//innerWidth;
 let h = canvas.height = 512;//innerHeight;
 
 let speeds = [];
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 32; i++) {
   speeds[i] = Math.random() + 1;
 }
 
@@ -15,3 +15,18 @@ function calcOffs(_w = 1) {
   let sum = offs.reduce((sum, n) => sum + n, 0);
   return offs.map(n => n / sum * _w);
 }
+
+c.fillStyle = '#fff';
+
+function draw() {
+  c.clearRect(0,0,w,h);
+
+  let offs = calcOffs(h);
+  for (let i = 0, len = offs.length; i < len; i++) {
+    c.fillRect(w * (i/len), 0, w/len, offs[i]);
+  }
+
+  requestAnimationFrame(draw);
+}
+
+draw();
