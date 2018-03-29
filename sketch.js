294708db65d170e32bf8ee22ae313bc1513fcfa5
Indigo Lily <yulex.42@gmail.com>
2018-03-29 15:32:35 -0300

    Created more chaotic wave to hide seam

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+"use strict";
 const canvas = document.getElementById('cnv');
 const c = canvas.getContext('2d');
 
@@ -13,7 +14,7 @@ window.addEventListener('load', () => {
   h = canvas.height = innerHeight;
 
   c.strokeStyle = '#fff';
-  c.lineWidth = Math.sqrt(2);
+  c.lineWidth = sqrt2;
   c.translate(w/2, h/2);
 
   r1 = Math.min(w,h)/2.5;
@@ -27,7 +28,7 @@ window.addEventListener('resize', () => {
   h = canvas.height = innerHeight;
 
   c.strokeStyle = '#fff';
-  c.lineWidth = Math.sqrt(2);
+  c.lineWidth = sqrt2;
   c.translate(w/2, h/2);
 
   r1 = Math.min(w,h)/2.5;
@@ -36,14 +37,22 @@ window.addEventListener('resize', () => {
 
 function calcOffs(_w = 1, _s = speeds) {
   let t = Date.now();
-  let offs = _s.map(s => Math.sin(t * s * 0.0015) + 1);
+  let offs = _s.map(s => sin(t * s * 0.001) + 1);
   let sum = offs.reduce((sum, n) => sum + n, 0);
   //return offs.map(n => 1/offs.length * _w); // evenly spaced for debugging
   return offs.map(n => n / sum * _w);
 }
 
 function polToCart(a, r) {
-  return [Math.sin(a) * r, Math.cos(a) * r];
+  return [sin(a) * r, Math.cos(a) * r];
+}
+
+const phi = (1+Math.sqrt(5))/2;
+const sqrt2 = Math.sqrt(2);
+const sin = Math.sin;
+
+function chaoticWave(x) {
+  return (sin(x) + sin(Math.PI * x) + sin(sqrt2 * x) + sin(phi * x))/4 * Math.PI*2;
 }
 
 function draw() {
@@ -61,8 +70,8 @@ function draw() {
   let total = 0;
   for (let i = 0, len = offs.length; i < len; i++) {
     c.beginPath();
-    c.moveTo(...polToCart(offs[i] + total + Math.sin(Math.sin(Math.sin(Date.now()/60000)*Math.PI*2)*Math.PI*2), r1));
-    c.lineTo(...polToCart(offs[i] + total + Math.sin(Math.sin(Math.sin(Date.now()/60000)*Math.PI*2)*Math.PI*2), r2));
+    c.moveTo(...polToCart(offs[i] + total + chaoticWave(Date.now()/60000), r1));
+    c.lineTo(...polToCart(offs[i] + total + chaoticWave(Date.now()/60000), r2));
     c.stroke();
     total += offs[i];
   }
